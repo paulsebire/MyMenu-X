01b1e478f4bf870900073296947ce0ac9eaa4f0f
cursan-a <anthony.cursan@contentsquare.com>
2018-03-01 10:40:16 +0900

    add tests for exceptions catching

--- test/com/ocr/anthony/OrderTest.java
+++ test/com/ocr/anthony/OrderTest.java
@@ -288,4 +288,22 @@ public class OrderTest {
                 "Vous avez choisi comme menu : boeuf%n" +
                 "Vous avez choisi comme accompagnement : frites%n" , order.orderSummary);
     }
+
+    @Test
+    public void Given_TextResponse_When_CallingAskQuestion_Then_DisplayErrorSentence() {
+        System.setIn(new ByteArrayInputStream(String.format("texte%n1%n").getBytes()));
+        order = new Order();
+        String[] responses = {"BMW", "Audi", "Mercedes"};
+        order.askSomething("voiture", responses);
+        String[] output = outContent.toString().replace("\r\n", "\n").split("\n");
+        assertEquals("Vous n'avez pas choisi un des choix proposés comme voiture", output[5]);
+    }
+    @Test
+    public void Given_BadMenusQuantityInStandardInput_When_MenusIsRun_Then_DisplayErrorSentence() {
+        System.setIn(new ByteArrayInputStream(String.format("texte%n1%n1%n2%n3%n").getBytes()));
+        order = new Order();
+        order.runMenus();
+        String[] output = outContent.toString().replace("\r\n", "\n").split("\n");
+        assertEquals("Vous n'avez pas choisi un nombre de menu valide", output[1]);
+    }
 }
